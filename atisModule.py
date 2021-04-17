a97a637f9f66fd234d1a4820aacb2ced32e83d29
Serpent03 <dumbpilot03@gmail.com>
2021-04-17 18:17:10 +0530

    Second commit. Added anime searching feature but WIP.

--- atisModule.py
+++ atisModule.py
@@ -12,8 +12,8 @@ import bs4
 def getWxData(ID):   #get website data
 
     dataURL = f'https://www.aviationweather.gov/metar/data?ids={ID}&format=raw&hours=0&taf=off&layout=on'
-    dataResponse = urllib.request.urlopen(dataURL)
-    webContent = dataResponse.read()    # result in type <byte>
+    dataHandle = urllib.request.urlopen(dataURL)
+    webContent = dataHandle.read()    # result in type <byte>
 
     webContent = str(webContent, 'utf-8')   # convert type <byte> to <str>
 
@@ -46,8 +46,8 @@ def getScrape(query):
             pass
 
         webRequest = requests.get(f"https://www.google.com/search?q={query}&sxsrf=ALeKk01RAkqXw-MoxUbvYpX1pgDWXDsS2g%3A1618557805015&ei=bTt5YPEoycutAaq4oagJ&oq={query}&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyAggAMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgcIABBHELADOgQIABBDOgQILhBDOgoILhDHARCvARBDOgcILhCxAxBDOgoIABCxAxCDARBDOgcIABCxAxBDOggILhDHARCvAToFCAAQkQJQwrEDWNrCA2DjxANoAXACeACAAeMCiAH-G5IBBzAuOC44LjGYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=gws-wiz&ved=0ahUKEwixk5DYnYLwAhXJZSsKHSpcCJUQ4dUDCA4&uact=5")
-        webContent = bs4.BeautifulSoup(webRequest.text, "html.parser")
-        webContent = str(webContent)    # convert to str
+        webProcess = bs4.BeautifulSoup(webRequest.text, "html.parser")
+        webContent = str(webProcess)    # convert to str
 
         return getWxData(webContent[webContent.find("ICAO")+6:webContent.find("ICAO")+10])
 
