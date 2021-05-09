68e1ea901d92e358f759dccba1b5ace372264b47
unknown <dumbpilot03@gmail.com>
2021-05-09 16:27:26 +0530

    Testing selenium

--- anime.py
+++ anime.py
@@ -2,10 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 
 def getAnime(query):
-    query = query.replace(" ", "%20")
+    query = query.replace(" ", "+")
 
 
-    webRequest = requests.get(f'https://www.google.com/search?q={query}')
+    webRequest = requests.get(f'https://www.google.com/search?q={query}&rlz=1C1RXQR_enIN951IN951&oq=Nvidia+control+panel+settings+FS2020&aqs=chrome..69i57.10328j0j4&sourceid=chrome&ie=UTF-8')
     #webRequest = requests.get("https://en.wikipedia.org/wiki/List_of_game_engines")
     webProcess = BeautifulSoup(webRequest.text, "html.parser")
 
